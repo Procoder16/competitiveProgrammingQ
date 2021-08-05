9a97b8ae7ebede3c6695c1f32fa9b64a27cfbc40
Procoder16 <smsoumik1608@gmail.com>
2021-08-05 22:14:15 +0530

    Tram codeforces added

---
+++ A_Tram.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define ff              first
+#define ss              second
+#define int             long long
+#define pb              push_back
+#define mp              make_pair
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define pqb             priority_queue<int>
+#define pqs             priority_queue<int,vi,greater<int> >
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define mod             1000000007
+#define inf             1e18
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n,type)  type *arr=new type[n];
+#define w(x)            int x; cin>>x; while(x--)
+mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
+ 
+int32_t main()
+{   
+    int maxC = 0;
+    int currentC = 0;
+    w(t){
+        int a, b;
+        cin>>a>>b;
+
+        currentC-=a;
+        currentC+=b;
+
+        maxC = max(maxC, currentC);
+    }
+
+    cout<<maxC;
+
+    return 0;
+}
