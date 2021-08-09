13f98922b229a588115bf03a640ab44c4ca580c4
Procoder16 <smsoumik1608@gmail.com>
2021-08-09 21:42:30 +0530

    young physicist added codeforces

--- A_Young_Physicist.cpp
+++ A_Young_Physicist.cpp
@@ -1,56 +1,27 @@
-#include<bits/stdc++.h>
-//#include <ext/pb_ds/assoc_container.hpp>
-//using namespace __gnu_pbds;
+#include <iostream>
+
 using namespace std;
- 
-#define ff              first
-#define ss              second
-#define int             long long
-#define pb              push_back
-#define mp              make_pair
-#define pii             pair<int,int>
-#define vi              vector<int>
-#define mii             map<int,int>
-#define pqb             priority_queue<int>
-#define pqs             priority_queue<int,vi,greater<int> >
-#define setbits(x)      __builtin_popcountll(x)
-#define zrobits(x)      __builtin_ctzll(x)
-#define mod             1000000007
-#define inf             1e18
-#define ps(x,y)         fixed<<setprecision(y)<<x
-#define mk(arr,n,type)  type *arr=new type[n];
-#define w(x)            int x; cin>>x; while(x--)
-mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
- 
-//typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
- 
- 
-// void c_p_c()
-// {
-//     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-// #ifndef ONLINE_JUDGE
-//     freopen("input.txt", "r", stdin);
-//     freopen("output.txt", "w", stdout);
-// #endif
-// }
- 
-int32_t main()
+
+int main()
 {
-    //c_p_c();
+    int n, x, y, z, xsum(0), ysum(0), zsum(0);
+    cin >> n;
 
-    int sum = 0;
-
-    w(t){
-        int x, y, z;
-        cin>>x>>y>>z;
-        sum+= x + y + z;
+    while (n--)
+    {
+        cin >> x >> y >> z;
+        xsum += x;
+        ysum += y;
+        zsum += z;
     }
 
-    if(sum == 0){
-        cout<<"YES"<<endl;
+    if (xsum == 0 && ysum == 0 && zsum == 0)
+    {
+        cout << "YES" << endl;
     }
-    else{
-        cout<<"NO"<<endl;
+    else
+    {
+        cout << "NO" << endl;
     }
 
     return 0;
