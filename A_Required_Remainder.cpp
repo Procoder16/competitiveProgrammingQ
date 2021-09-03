b9113b8257d7e1ad8928539a6d75d03adc4c0625
Procoder16 <smsoumik1608@gmail.com>
2021-09-03 21:27:23 +0530

    required remainder codeforces added

---
+++ A_Required_Remainder.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+    int x, y, n, t;
+
+    cin >> t;
+
+    while (t--)
+
+    {
+
+        cin >> x >> y >> n;
+
+        cout << (n - y) / x * x + y << endl;
+    }
+}
