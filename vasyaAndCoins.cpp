71b5b7b699eaa725b613a72bde17333ddee0d1e6
Procoder16 <smsoumik1608@gmail.com>
2022-04-01 21:32:06 +0530

    more questions added

---
+++ vasyaAndCoins.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define ff              first
+#define ss              second
+#define int             long long
+#define pb              push_back
+#define mp              make_pair
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define pqb             priority_queue<int>
+#define pqs             priority_queue<int,vi,greater<int> >
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define mod             1000000007
+#define inf             1e18
+#define ps(x,y)         fixed<<setprecision(y)<<xz
+#define mk(arr,n,type)  type *arr=new type[n];
+#define w(x)            int x; cin>>x; while(x--)
+mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+void c_p_c()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solution(){
+    int a, b;
+    cin>>a>>b;
+    int ans;
+
+    if(a == 0 && b != 0){
+        ans = 1;
+        cout<<ans<<endl;
+    }
+    else{
+        ans = a + (2*b) + 1;
+        cout<<ans<<endl;
+    }
+}
+ 
+int32_t main()
+{
+    c_p_c();
+    int t;
+    cin>>t;
+    while(t--){
+        solution();    
+    }    
+
+    return 0;
+}
---
+++ vladAndCandies.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define ff              first
+#define ss              second
+#define int             long long
+#define pb              push_back
+#define mp              make_pair
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define pqb             priority_queue<int>
+#define pqs             priority_queue<int,vi,greater<int> >
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define mod             1000000007
+#define inf             1e18
+#define ps(x,y)         fixed<<setprecision(y)<<xz
+#define mk(arr,n,type)  type *arr=new type[n];
+#define w(x)            int x; cin>>x; while(x--)
+mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+void c_p_c()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solution(){
+    int n;
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        cin>>arr[i];
+    }
+
+    if(n == 1 && arr[0] > 1){
+        cout<<"NO"<<endl;
+    }
+
+    sort(arr, arr+n);
+
+    for(int i = n-1; i >= 1; i--){
+        if(arr[i] - arr[i-1] > 1){
+            cout<<"NO"<<endl;
+            return;
+        }
+        arr[i-1] = arr[i] - arr[i-1];
+    }
+    cout<<"YES"<<endl;
+}
+ 
+int32_t main()
+{
+    c_p_c();
+    int t;
+    cin>>t;
+    while(t--){
+        solution();    
+    }    
+
+    return 0;
+}
+
